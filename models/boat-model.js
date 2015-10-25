7b5584065056ff7698ce7280a1784940b25c8fed
msudalaiyandi <msudalaiyandi@paypal.com>
2015-10-25 03:54:12 -0700

    Order flow error scenarios handling issue fixed.

--- models/boat-model.js
+++ models/boat-model.js
@@ -22,72 +22,94 @@ module.exports = {
         // Get Customer and Device info by mac_id.
         request.get({
             url: getCustomerAndDeviceByMacIdUrl
-        }, function(err, res, body) {
+        }, function(err, res, custInfoBody) {
 
-            console.log('Customer and Device body : ' + JSON.stringify(body));
+            console.log('Customer and Device body : ' + JSON.stringify(custInfoBody));
 
-            body = JSON.parse(body);
+            // Customer-Device Error scenario.
+            if(err || !res || res.statusCode>=300) {
+                req.model = {
+                    status: 'failed',
+                    reason: 'Unable to fetch customer and device info.'
+                }
+                next();
+            } else {
+                custInfoBody = JSON.parse(custInfoBody);
+                console.log('Customer : ' + JSON.stringify(custInfoBody.customer));
+                var customer = body && body.customer,
+                    bt_customer_id = customer && customer.bt_customer_id,
+                    paymentMethodToken = customer && customer.payment_method_token,
+                    itemId = custInfoBody && custInfoBody.device && custInfoBody.device.item_id;
 
-            console.log('Customer : ' + JSON.stringify(body.customer));
+                console.log('Item Id : ' + itemId);
 
-            var customer = body && body.customer,
-                bt_customer_id = customer && customer.bt_customer_id,
-                paymentMethodToken = customer && customer.payment_method_token,
-                itemId = body && body.device && body.device.item_id;
+                if(itemId) {
 
-            if(itemId) {
+                    // Get item info by itemId.
+                    var getItemByItemIdUrl = req.app.kraken.get('urls').getItemByItemIdUrl + '/' + itemId;
 
-                // Get item info by itemId.
-                var getItemByItemIdUrl = req.app.kraken.get('urls').getItemByItemIdUrl + '/' + itemId;
-                request.get({
-                    url: getItemByItemIdUrl
-                }, function (err, res, body) {
+                    request.get({
+                        url: getItemByItemIdUrl
+                    }, function (err, res, itemBody) {
 
-                    console.log('Item - body : ' + JSON.stringify(body));
+                        console.log('Item - body : ' + JSON.stringify(itemBody));
 
-                    body = JSON.parse(body);
+                        if(err || !res || res.statusCode>=300 || !itemBody) {
 
-                    var item_upc = body && body.item_upc;
+                            req.model = {
+                                status: 'failed',
+                                reason: 'Unable to fetch item info.'
+                            }
+                            next();
+                        } else {
+                            itemBody = JSON.parse(itemBody);
 
-                    //Post customer and item details to create order.
-                    var createOrderUrl = req.app.kraken.get('urls').createOrderUrl;
+                            var item_upc = itemBody && itemBody.item_upc;
 
-                    request.post({
-                        url: createOrderUrl,
-                        body: JSON.stringify({
-                            bt_customer_id: bt_customer_id,
-                            payment_method_token: paymentMethodToken,
-                            item_upc: item_upc
-                        }),
-                        headers: {
-                            'Content-Type': 'application/json'
+                            console.log('item_upc : ' + item_upc);
+
+                            if(item_upc) {
+
+                                //Post customer and item details to create order.
+                                var createOrderUrl = req.app.kraken.get('urls').createOrderUrl,
+                                    payload = {
+                                        bt_customer_id: bt_customer_id,
+                                        payment_method_token: paymentMethodToken,
+                                        item_upc: item_upc
+                                    };
+                                console.log('Create Order - Payload : ' + JSON.stringify(payload));
+
+                                request.post({
+                                    url: createOrderUrl,
+                                    body: JSON.stringify(payload),
+                                    headers: {
+                                        'Content-Type': 'application/json'
+                                    }
+                                }, function (err, res, orderBody) {
+                                    if(err || !res || res.statusCode>=300 || !orderBody) {
+                                        req.model = {
+                                            status: 'failed',
+                                            reason: 'Unable to fetch item info.'
+                                        }
+                                    } else {
+                                        req.model = {
+                                            status: 'success',
+                                            reason: 'Order placed successfully'
+                                        }
+                                    }
+                                    next();
+                                });
+                            }
                         }
-                    }, function (err, res, body) {
-                       console.log('Error : ' + err);
-                        console.log('Error : ' + res);
-                        console.log('Error : ' + body);
-                        next();
                     });
-                });
-
+                } else {
+                    req.model = {
+                        status: 'failed',
+                        reason: 'Item Id is not available.'
+                    }
+                    next();
+                }
             }
-
-
-
-
-
-
-
-
-
         });
-
-
-
-
-
-
-
     }
-
 };
